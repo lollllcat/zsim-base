573ab779b6b2f5ec4781af6056141379fbfc1136
lollllcat <44036734+lollllcat@users.noreply.github.com>
2020-04-23 15:24:46 -0700

    Update pin_cmd.cpp
    to allow pin work on higher Linux kernel version.

--- src/pin_cmd.cpp
+++ src/pin_cmd.cpp
@@ -58,6 +58,7 @@ PinCmd::PinCmd(Config* conf, const char* configFile, const char* outputDir, uint
     args.push_back("-follow_execv"); //instrument child processes
     args.push_back("-tool_exit_timeout"); //don't wait much of internal threads
     args.push_back("1");
+    args.push_back("-ifeellucky");
 
     //Additional options (e.g., -smc_strict for Java), parsed from config
     const char* pinOptions = conf->get<const char*>("sim.pinOptions", "");
